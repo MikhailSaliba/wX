43041736b8da4d74e71bd2bfdffe3373de0fcf46
Mika-Tweak <mikhail.saliba99@gmail.com>
2024-06-26 23:18:28 -0700

    eslinn't v3

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+    module.exports = {
+        eslint: {
+          // Warning: This allows production builds to successfully complete even if
+          // your project has ESLint errors.
+          ignoreDuringBuilds: true,
+        },
+      }
+    
+};
+
 
 export default nextConfig;
